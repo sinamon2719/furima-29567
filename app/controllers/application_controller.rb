9c13f17735789f563c519cc2782a3e4a6e042bfd
sinamon2719 <okigeisai2012@yahoo.co.jo>
2020-09-10 20:07:56 +0900

    ログイン機能実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,4 +5,7 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname,:email,:family_name,:first_name,:family_name_kana,:first_name_kana,:birth_day])
   end
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_in, keys: [:email])
+  end
 end
