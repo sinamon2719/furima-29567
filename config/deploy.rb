812d4cbfc1d58e6757df1231cd4b210800bad8e0
sinamon2719 <okigeisai2012@yahoo.co.jo>
2020-09-24 22:45:23 +0900

    修正

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,7 +5,7 @@ lock '3.14.1'
 set :application, 'furima-29567'
 
 # どのリポジトリからアプリをpullするかを指定する
-set :repo_url,  'git@github.com:sinamon2719/furima29567.git'
+set :repo_url,  'git@github.com:sinamon2719/furima-29567.git'
 
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
