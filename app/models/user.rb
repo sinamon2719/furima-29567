2190fedd60fdfa24ce749cf093b991f5dbdd0413
sinamon2719 <okigeisai2012@yahoo.co.jo>
2020-09-28 10:39:56 +0900

    facebook　Google導入

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,23 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+        :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: [:facebook, :google_oauth2]
+
   has_many :items
-  has_many :sns_credentials
+  has_many :sns_credentials 
+  def self.from_omniauth(auth)
+    sns = SnsCredential.where(provider: auth.provider, uid: auth.uid).first_or_create
+    user = User.where(email: auth.info.email).first_or_initialize(
+      nickname: auth.info.name,
+        email: auth.info.email
+    )
+    # userが登録済みであるか判断
+   if user.persisted?
+    sns.user = user
+    sns.save
+  end
+  { user: user, sns: sns }
+  end
 
   validates :nickname, presence: true
   validates :email, presence: true
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_18_040338) do
+ActiveRecord::Schema.define(version: 2020_09_27_120120) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -59,6 +59,15 @@ ActiveRecord::Schema.define(version: 2020_09_18_040338) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "sns_credentials", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "provider"
+    t.string "uid"
+    t.bigint "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_sns_credentials_on_user_id"
+  end
+
   create_table "user_items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "user_id"
     t.integer "item_id"
@@ -85,4 +94,5 @@ ActiveRecord::Schema.define(version: 2020_09_18_040338) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "sns_credentials", "users"
 end
